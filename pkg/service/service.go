2a65531763827c4029017725e89ec183cfabe014
m.maruschak <m.maruschak@rozetkapay.com>
2024-07-14 16:30:30 +0300

    init

---
+++ pkg/service/service.go
@@ -0,0 +1,25 @@
+package service
+
+/*
+
+Service drive every logic inside, what business logic for Auth Service.
+1. Register
+2. Auth (Basic login and password, OAuth) + 2FA
+3. JWT token to other services to control policy and view
+
+More about Auth Flows:
+Access Management
+Authentication
+Credential Management
+Data Security
+Multi-Factor Authentication
+Password Management
+Single Sign On
+Two-Factor Authentication
+User Management
+
+*/
+
+func New() error {
+	return nil
+}
